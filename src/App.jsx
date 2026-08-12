c0a069cba7112eb155730af4780d1c8ccaf84462
Vinit Kumar Singh <VinitSingh.dev@gmail.com>
2026-08-12 23:06:12 +0530

    Debugging

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import Header from "./components/Header/Header";
+import {getProducts} from "./api/api";
+import { useEffect } from "react";
 
 const App = () => {
+
+  useEffect(()=>{
+    const getHomeProducts=async()=>{
+      try {
+        const data=await getProducts();
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getHomeProducts();
+  },[]);
   return (
     <div>
       <Header/>
---
+++ src/api/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const API_URL = import.meta.env.VITE_API_URL;
+
+export const getProducts=async()=>{
+    const response=await axios.get(`${API_URL}/products`);
+    return response.data.products;
+}
