1b92c0478bc7759cd465a147e50976aa632975ec
Vinit Kumar Singh <VinitSingh.dev@gmail.com>
2026-08-20 23:33:53 +0530

    Debugging

--- src/api/api.js
+++ src/api/api.js
@@ -4,5 +4,6 @@ export const API_URL = import.meta.env.VITE_API_URL;
 
 export const getProducts=async()=>{
     const response=await axios.get(`${API_URL}/products`);
+    console.log(response.data.products);
     return response.data.products;
 }
